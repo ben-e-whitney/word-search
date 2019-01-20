48ecac9c044ab3a1d0f4457e972d95ae9865e7ac
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2019-01-20 16:27:43 -0500

    Add `WordMatchStarter` structure.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
+struct WordMatchStarter {
+    const char *word;
+    size_t length;
+    const char *start;
+    signed char step;
+};
+
+void initialize_word_match_starter(
+    struct WordMatchStarter * const p,
+    const char * const word,
+    const size_t length,
+    const char * const start,
+    const signed char step
+    ) {
+    p->word = word;
+    p->length = length;
+    p->start = start;
+    p->step = step;
+}
+
+enum direction {SOUTHWEST, SOUTH, SOUTHEAST, EAST};
+
+struct WordMatch {
+    struct WordMatchStarter * const starter;
+    unsigned int row;
+    unsigned int col;
+    enum direction heading;
+};
 
 int main(void) {
     char *line = NULL;
@@ -16,7 +46,7 @@ int main(void) {
     unsigned int ncols;
     unsigned int nwords;
     {
-        int nvar = sscanf(line, "%u %u %u\n", &nrows, &ncols, &nwords);
+        const int nvar = sscanf(line, "%u %u %u\n", &nrows, &ncols, &nwords);
         if (nvar != 3) {
             fprintf(
                     stderr,
@@ -28,20 +58,30 @@ int main(void) {
     }
     free(line);
 
-    char **words = (char **) malloc(nwords * sizeof(char *));
+    struct WordMatchStarter *starters = (struct WordMatchStarter *) malloc(
+        2 * nwords * sizeof(struct WordMatchStarter)
+    );
+    unsigned int letter_counts[26];
+    for (unsigned int i = 0; i < 26; ++i) {
+        letter_counts[i] = 0;
+    }
+
+    char ** const words = (char ** const) malloc(nwords * sizeof(char *));
     for (unsigned int i = 0; i < nwords; ++i) {
         words[i] = NULL;
         size_t n = 0;
-        ssize_t nbytes = getline(words + i, &n, stdin);
+        const ssize_t nbytes = getline(words + i, &n, stdin);
         if (nbytes == -1) {
             fprintf(stderr, "failed to read word %u\n", i);
             exit(1);
         }
-        char *p = words[i];
+        char * const p = words[i];
+        unsigned int length = 0;
         for (unsigned int j = 0; j < n; ++j) {
-            char c = p[j];
+            const char c = p[j];
             if (c == '\n') {
                 p[j] = '\0';
+                length = j;
                 break;
             } else if (!isupper(c)) {
                 fprintf(
@@ -50,6 +90,42 @@ int main(void) {
                 exit(1);
             }
         }
+        if (!length) {
+            fprintf(stderr, "word %u: word is empty\n", i);
+            exit(1);
+        }
+        ++letter_counts[p[0] - 'A'];
+        ++letter_counts[p[length - 1] - 'A'];
+    }
+
+    struct WordMatchStarter *letter_starts[26];
+    struct WordMatchStarter *p = starters;
+    for (unsigned int i = 0; i < 26; ++i) {
+        letter_starts[i] = p;
+        p += letter_counts[i];
+        /*`letter_counts[i]` will now be used to tell us how many
+         * `struct WordMatchStarter`s have been written for each letter.*/
+        letter_counts[i] = 0;
+    }
+
+    for (unsigned int i = 0; i < nwords; ++i) {
+        const char *start;
+        unsigned int j;
+        struct WordMatchStarter * p;
+        const char * const word = words[i];
+        const size_t length = strlen(word);
+
+        start = word;
+        j = *start - 'A';
+        p = letter_starts[j] + letter_counts[j];
+        initialize_word_match_starter(p, word, length, start, 1);
+        ++letter_counts[j];
+
+        start = word + (length - 1);
+        j = *start - 'A';
+        p = letter_starts[j] + letter_counts[j];
+        initialize_word_match_starter(p, word, length, start, -1);
+        ++letter_counts[j];
     }
 
     for (unsigned int i = 0; i < nrows; ++i) {
@@ -75,6 +151,7 @@ int main(void) {
                 );
                 exit(1);
             }
+            unsigned int k = c - 'A';
         }
         c = fgetc(stdin);
         if (c == EOF) {
